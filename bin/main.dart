e0cd4fe6139223ca58ba8d706c1f284dcb32d061
Chris Ian Fiel <ccfiel@gmail.com>
2019-12-18 17:15:42 +0800

    create app

--- bin/main.dart
+++ bin/main.dart
@@ -2,7 +2,7 @@ import 'package:bai/create.dart' as create;
 
 void main(List<String> arguments) async {
   print(arguments);
-  if (arguments[0] == 'create') {
+  if (arguments[0] == 'new') {
     await create.create(arguments[1], arguments[1]);
   }
 }
