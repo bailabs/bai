db01e51d9c30483b0876c3a8ea2f687662fca71b
Chris Ian Fiel <ccfiel@gmail.com>
2019-12-18 15:32:19 +0800

    create app

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,21 @@
+import 'dart:io';
+
 import 'package:bai/create.dart' as create;
 
-void main(List<String> arguments) {
+bool isDirectoryExist(dirName) {
+  return FileSystemEntity.typeSync(dirName) != FileSystemEntityType.notFound;
+}
+
+void main(List<String> arguments) async {
   print(arguments);
-  if (arguments[0] == 'create') {}
-  print('Hello world: ${create.create()}!');
+  if (arguments[0] == 'create') {
+    if (!isDirectoryExist(arguments[1])) {
+      if (await create.create(arguments[1], arguments[1]) != 0) {
+        print('Error downloading template');
+        exit(0);
+      }
+    } else {
+      print('App already exist');
+    }
+  }
 }
--- lib/create.dart
+++ lib/create.dart
@@ -1,7 +1,26 @@
 import 'dart:io';
 
-Future<String> create() async {
-  return await Process.run(
-          'git', ['clone', 'https://github.com/bailabs/clean_template.git'])
-      .toString();
+Future<int> create(String dirName, String appName) async {
+  var gitClone = await Process.run('git',
+      ['clone', 'https://github.com/bailabs/clean_template.git', dirName]);
+
+  var fileNames = [
+    '${dirName}/android/app/src/main/AndroidManifest.xml',
+    '${dirName}/ios/Runner/Info.plist',
+    '${dirName}/lib/main.dart'
+  ];
+  for (var fileName in fileNames) {
+    changeAppName(fileName, appName);
+  }
+  return gitClone.exitCode;
+}
+
+void changeAppName(fileName, appName) async {
+  var file = File(fileName);
+  String contents;
+  if (await file.exists()) {
+    contents = await file.readAsString();
+    var newContent = contents.replaceAll('{clean_template}', appName);
+    await File(fileName).writeAsString(newContent);
+  }
 }
