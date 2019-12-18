af226529f8583a53531b24b218e37a724d9ca492
Chris Ian Fiel <ccfiel@gmail.com>
2019-12-18 17:03:10 +0800

    create app

--- lib/create.dart
+++ lib/create.dart
@@ -1,18 +1,39 @@
 import 'dart:io';
 
-Future<int> create(String dirName, String appName) async {
+bool isDirectoryExist(dirName) {
+  return FileSystemEntity.typeSync(dirName) != FileSystemEntityType.notFound;
+}
+
+Future<bool> create(String dirName, String appName) async {
+  if (isDirectoryExist(dirName)) {
+    print('${dirName} app already exist');
+  }
   var gitClone = await Process.run('git',
       ['clone', 'https://github.com/bailabs/clean_template.git', dirName]);
+
+  if (gitClone.exitCode != 0) {
+    print('Error downloading template');
+  }
 
   var fileNames = [
     '${dirName}/android/app/src/main/AndroidManifest.xml',
     '${dirName}/ios/Runner/Info.plist',
     '${dirName}/lib/main.dart'
   ];
+
   for (var fileName in fileNames) {
-    changeAppName(fileName, appName);
+    await changeAppName(fileName, appName);
   }
-  return gitClone.exitCode;
+
+  await Directory('${dirName}/.git').delete(recursive: true);
+  Directory.current = Directory('${dirName}/');
+  var pubGet = await Process.run('flutter', ['pub', 'get']);
+
+  if (pubGet.exitCode != 0) {
+    print('Error updating packages');
+  }
+
+  return gitClone.exitCode != 0 && pubGet.exitCode != 0;
 }
 
 void changeAppName(fileName, appName) async {
