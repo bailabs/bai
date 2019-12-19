20e8f1a13659764fbb07570b9945710ba8a9e876
Chris Ian Fiel <ccfiel@gmail.com>
2019-12-19 17:48:32 +0800

    create app

--- bin/main.dart
+++ bin/main.dart
@@ -1,13 +1,21 @@
-import 'dart:io';
-
 import 'package:bai/create.dart' as create;
 import 'package:bai/page.dart';
+import 'package:recase/recase.dart';
+
+String generateName(String name) {
+  var rc = ReCase(name);
+  return rc.titleCase.replaceAll(' ', '');
+}
 
 void main(List<String> arguments) async {
   var command = arguments[0].trim();
   if (command == 'new') {
     await create.create(arguments[1], arguments[1]);
   } else if (command == 'g') {
-    await File('test.dart').writeAsString(test1());
+    var action = arguments[1].trim();
+    if (action == 'page') {
+      var pageName = generateName(arguments[2].trim());
+      await generatePage(pageName);
+    }
   }
 }
--- lib/page.dart
+++ lib/page.dart
@@ -1,18 +1,20 @@
-import 'package:code_builder/code_builder.dart';
-import 'package:dart_style/dart_style.dart';
+import 'dart:async';
+import 'dart:io';
 
-final _dartfmt = DartFormatter();
+import 'package:recase/recase.dart';
 
-String test1() {
-  final animal = Class((b) => b
-    ..name = 'Animal'
-    ..extend = refer('Organism')
-    ..methods.add(Method.returnsVoid(
-      (b) => b
-        ..name = 'eat'
-        ..annotations
-        ..body = const Code(''),
-    )));
-  final emitter = DartEmitter();
-  return DartFormatter().format('${animal.accept(emitter)}');
+Future<bool> generatePage(String name) async {
+  var rc = ReCase(name);
+  var pageNameSmall = rc.snakeCase;
+
+  var dir = Directory('lib/template/page/');
+  var entries = dir.listSync(recursive: false).toList();
+  for (File entry in entries) {
+    var content = await entry.readAsString();
+    var content1 = content.replaceAll('{pageName}', name);
+    var content2 = content1.replaceAll('{pageNameSmall}', pageNameSmall);
+
+    print(content2);
+  }
+  return true;
 }
