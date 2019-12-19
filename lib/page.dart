8ee2d87a45cd25310526fcb9859b7daf66ad1cea
Chris Ian Fiel <ccfiel@gmail.com>
2019-12-19 18:05:32 +0800

    create app

--- lib/page.dart
+++ lib/page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:path/path.dart';
 import 'package:recase/recase.dart';
 
 Future<bool> generatePage(String name) async {
@@ -10,11 +11,12 @@ Future<bool> generatePage(String name) async {
   var dir = Directory('lib/template/page/');
   var entries = dir.listSync(recursive: false).toList();
   for (File entry in entries) {
+    var filename = basenameWithoutExtension(entry.path);
     var content = await entry.readAsString();
     var content1 = content.replaceAll('{pageName}', name);
     var content2 = content1.replaceAll('{pageNameSmall}', pageNameSmall);
-
-    print(content2);
+    await File(pageNameSmall + '_${filename}' + '.dart')
+        .writeAsString(content2);
   }
   return true;
 }
